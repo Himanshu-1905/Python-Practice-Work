bbc296fbb89127ea2d70fd99453f740325fa2b73
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-25 10:26:01 +0530

    Add files via upload

---
+++ Dictionary_Ex.py
@@ -0,0 +1,13 @@
+# Create an empty dictionary. Allow 4 friends to enter favourite language as values and use keys as their names. Assume that the names are unique.
+
+favLang = {}
+a = input("Enter your favorite language Himanshu\n")
+b = input("Enter your favorite language Jatin\n")
+c = input("Enter your favorite language Harsh\n")
+d = input("Enter your favorite language Rahul\n")
+favLang['Himanshu'] = a
+favLang['Jatin'] = b
+favLang['Harsh'] = c
+favLang['Rahul'] = d
+
+print(favLang)
