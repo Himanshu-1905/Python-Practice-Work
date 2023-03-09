9f8ce142df2d9477d632ecac73644d2aa8509cca
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-09 20:24:39 +0530

    Add files via upload

---
+++ Multiplication_table_using_for_loop_in_reversed_order.py
@@ -0,0 +1,8 @@
+# Write a program to print multiplication table of n using for loop in reversed order.
+
+num = int(input("enter the number= "))
+i=10
+while i>=1:
+    print(num,"X",i,"=",num*i)
+    i= i-1
+
