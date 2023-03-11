f239d0d8971fa048da385fc760c0a4913b52b059
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-11 09:32:15 +0530

    Add files via upload

---
+++ Convert_Celsius_to_Fahrenheit_using_function.py
@@ -0,0 +1,8 @@
+# write a python program using function to convert Celsius to Fahrenheit
+
+def farh(cel):
+    return (cel *(9/5)) + 32
+
+c = 0
+f = farh(c)
+print("Fahreheit Temperature is " + str(f))
